51b49302a0cee96c0a8ba5a956a37c542d7920f6
User <tinwald@gmail.com>
2018-08-18 13:54:04 +0800

    added promises support for multiple address handling

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app           = express()
 const port    = process.env.PORT || 8080;
 
 // convert to read this from Env setting
-const deposit_address_list = ['0xa0d7eF69cEBA8df2EFB11FdaFD53571C5CA710C2'];
+let deposit_address_list = getAddressList();
 const update_url = 'https://api.abelegroup.io/monitoring/update_transaction';
 
 // parse application/json
@@ -28,60 +28,84 @@ app.get('/', function(req, res) {
 app.post('/transaction/update', function(req, res) {
   let count = 0;
   let total = 0;
+  const errors = [];
+  const promises = [];
   for (var address of deposit_address_list) {
     let url = "http://api.ethplorer.io/getAddressTransactions/"+address+"?apiKey=freekey";
-    console.log("processing deposit address "+address);
+    const addy = address;
+    console.log("processing deposit address "+addy);
     var options = { uri: url, json: true };
-    rp(options)
-    .then(function(body) {
-      if (body && body.length > 0) {
-        for (var txn of body) {
-          const sender = txn.from;
-          const hash = txn.hash;
-          const amount = txn.value;
-          count++;
-          total += amount;
-          request.post( update_url , { currency: 'ETH', amount: amount, wallet_address: sender, tx_hash: hash },
+    promises.push(rp(options)
+      .then(function(body) {
+        if (body && body.length > 0) {
+          for (var txn of body) {
+            let data = {};
+            data["wallet_address"] = txn.from;
+            data["tx_id"] = txn.hash;
+            data["tx_hash"] = txn.hash;
+            data["amount"] = txn.value;
+            data["currency"] = 'ETH';
+            count++;
+            total += txn.value;;
+            request.post({
+              url: update_url,
+              method: "POST",
+              json: true,
+              body: data
+            },
             function (error, response, body) {
-              if (!error && response.statusCode == 200) {
-                console.log(body)
+              if (response.statusCode == 200) {
+                console.log("Updated "+txn.hash+ " Successfully for sending wallet"+txn.from);
               } else {
-                console.log("Error " +response.statusCode+"  while updating sending wallet "+sender+" for currency ETH and amount "+amount);
+                console.log("Update of txn "+txn.hash+ " Failed wallet"+txn.from+" Status was "+response.statusCode);
+                errors.push("Error " +response.statusCode+"  while updating wallet "+error);
               }
-            }
-          );
+            });
+          }
+          console.log("Process "+count+" transactions for a total of "+total+" ETH");
+        } else {
+          errors.push("No transactions for address "+addy+" at ts "+new Date());
         }
-        console.log("Process "+count+" transactions for a total of "+total+" ETH");
-      } else {
-        console.log("No transactions for address "+address+" at ts "+new Date());
-      }
-    })
-    .catch(function (err) {
-      res.send({ status: 500 });
-    });
-  }
+      })
+      .catch(function (err) {
+        errors.push("Error processing wallet updates "+err);
+      })
+    );
+  } 
+  Promise.all(promises)
+  .then(function(error, result) {
+     console.log(errors);
+     if (errors && errors.length > 0) {
+       res.send({ status: 500, error: errors });
+     } else {
+       res.send({ status: 200, error: errors });
+     }
+  });
 });
 
-//
-// Retrieve total transactions sent to ETH address
-//
-app.get('/transaction/total', function(req, res) {
-    var options = {
-       uri: "https://api.etherscan.io/api?module=account&action=balance&address=" + ETH_ADDR + "&tag=latest&apikey=" + apikey,
-       json: true
-    };
-    rp(options).then(function(body) {
-        const total = body.result;
-        const ts = +new Date()
-        res.json({"currency": "ETH","total": total, "timestamp": ts});
-    })
-    .catch(function (err) {
-        res.status(500);
-    });
-});
+function getAddressList() {
+  let list;
+  if (process.env.ETH_ADDRESS_LIST) {
+    try {
+      list = process.env.ETH_ADDRESS_LIST;
+      if (list) {
+        list = list.split(',');
+        console.log("ETH LIST is a LIST ..."+list);
+      }
+    }
+    catch (err) {
+      console.log(err);
+      throw err;
+    }
+  } else {
+    throw "ETH Address list doesnt exist in process.env ...";
+  }
+  return list;
+}
 
 // Start the app listening to default port
 app.listen(port, function() {
    const name = process.env.HEROKU_APP_NAME || 'Unknown'
    console.log(name + ' app is running on port ' + port);
+   console.log("ETH Addy List is "+deposit_address_list);
 });
